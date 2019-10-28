6b681dce57ce8fc9ec716c397ab9e06ddd0055ed
Taissia George <taissia.george@khk.ee>
2019-10-28 11:27:21 +0200

    numbrid.sort, numbrite sorteerimine

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ console.log('JS test')
 
 //for tsükkel
 //üldtsükkel
-let autod = ['Kia', 'Toyota', 'Ford', 'Nissan'];
-//väljastus
-for(let i = 0; i < autod.length; i++){
-    console.log(autod[i]);
+let numbrid = [];
+//lisame nimekirja sisse juhusliku numbri
+for(let i = 0; i < 10; i++){
+    let arv = Math.floor(Math.random() * 10);
+    numbrid.push(arv);
 }
+//kontrollime tulemus
+console.log(numbrid);
+//sorteerime kasvujärjestuses
+numbrid.sort();
+//kontrollime tulemus
+console.log(numbrid)
+//for(let i = 0; i < numbrid.length; i++){
+  //  console.log(numbrid[i]);
+//}
