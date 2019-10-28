b936d7fc8af55792d5e5a89328404ad379b648bd
Taissia George <taissia.george@khk.ee>
2019-10-28 11:12:12 +0200

    for nimekirja töötlused

--- app.js
+++ app.js
@@ -3,20 +3,8 @@ console.log('JS test')
 
 //for tsükkel
 //üldtsükkel
-let lause;
-for(arv = 1; arv <= 10; arv ++){
-    lause = 'arv = ';
-    if(arv < 10) {
-        lause = lause + ' ';
-} 
-lause = lause + arv;
-if(arv % 2 == 0){
-    lause = lause + ' - paaris';
-} else{
-    lause = lause + ' - paaritu';
-}
-    console.log (lause);
-    if(arv >= 5) {
-        break;
-    }
+let autod = ['Kia', 'Toyota', 'Ford', 'Nissan'];
+//väljastus
+for(lit i = 0; i < autod.length; i++){
+    console.log(autod [i]);
 }
