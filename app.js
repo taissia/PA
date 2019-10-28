bbb22d7e711a16e7d3c1a5f7750edf508e679df7
Taissia George <taissia.george@khk.ee>
2019-10-28 10:44:59 +0200

    tsükkel, arvud 1-10

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log('JS test')
+//tsüklid
+
+//for tsükkel
+//üldtsükkel
+let arv;
+for(arv = 1; arv <= 10; arv ++){
+    console.log('arv = ' + arv);
+    if(arv < 10) {
+} else {
+    console.log ('arv = ' + arv)
+}
+}
