8f56a274abfcecfe7bc9b088e9e086d73c791526
Taissia George <taissia.george@khk.ee>
2019-10-28 11:41:03 +0200

    while, eelkontrollimisega tsükkel

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
 console.log('JS test')
 //tsüklid
 
-//for tsükkel
-//üldtsükkel
-let numbrid = [];
-//lisame nimekirja sisse juhusliku numbri
-for(let i = 0; i < 10; i++){
-    let arv = Math.floor(Math.random() * 10);
-    numbrid.push(arv);
+//while
+//ellkontrollimisega tsükkel
+let number = 1;
+while (number <= 10) {
+    console.log(number);
+  //suurendame juhtimismuutuja väärtus  
+    number++;
 }
-//kontrollime tulemus
-console.log(numbrid);
-//sorteerime kasvujärjestuses
-//numbrid.sort();
-
-//kontrollime tulemus
-console.log(numbrid)
-for(let kord = 0; kord < numbrid.length; kord++){
-    for(let i = 0; i < numbrid.length; i++){
-        let abi;
-        if (numbrid [i] > numbrid [i+1]){
-            abi = numbrid[i+1];
-            numbrid[i] = numbrid[i+1];
-            numbrid[i+1] = abi;
-            }
-    
-    }
-}
-//kontrollime tulemus
-console.log(numbrid);
