cbe57f91ef389f14cb36550ea369c15fe01143d0
Taissia George <taissia.george@khk.ee>
2019-11-05 15:18:17 +0200

    funktsioon, mis väljastab tulemus ekraani

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 // Funktsioonid
 //Risküliku pindala arvutamine
 function pindalaArvutamine(pikkus, laius){
-    //väljastame väärtuse
-    console.log('Pikkus = ' + pikkus);
-    console.log('Laius = ' + laius);
     //arvutame pindala
     let pindala = pikkus * laius;
+    //tagstame pindala põhiprogrammi
+    return pindala;
+    
+}
+
+//funktsioon, mis väljastab tulemus ekraani
+function valjasta(pikkus, laius){
+    //väljastame väärtus
+    console.log('Pikkus = ' + pikkus);
+    console.log('Laius = ' + laius);
+    //salvestame arvutatud pindala
+    let pindala = pindalaArvutamine(pikkus, laius);
+    //väljastame tulemus
     console.log('Pindala = ' + pindala);
 }
-pindalaArvutamine(5, 6);
+
+valjasta(15, 5);
+valjasta(154, 587);
