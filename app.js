abe2f51e3f7728a4978cda63ca7fdd5912280758
Taissia George <taissia.george@khk.ee>
2019-11-05 15:06:38 +0200

    Pindala arvutamine

--- app.js
+++ app.js
@@ -1,30 +1,11 @@
 // Funktsioonid
-//funktsiooni defineerimine
-// function funktsiooniNimi([param1, param2, ...,paramN]){
-    //funktsiooni sisu }
-function paarsuseKontroll(arv){
-    //tulemuslause gefineerimine
-    let lause;
-
-    //arvutame arv 2 jagamisel
-    let jaak = arv % 2;
-    //kui jaak võrdne nulliga
-    if( jaak === 0 ){
-        //tegu on paaris arvuga
-     
-        lause = arv + ' on paaris';
-    } else {
-        //tegu on paaritu arvuga
-        lause = arv + ' on paaritu ';
-    }
-    //tagastame antud väärtus
-    return lause;
+//Risküliku pindala arvutamine
+function pindalaArvutamine(pikkus, laius){
+    //väljastame väärtuse
+    console.log('Pikkus = ' + pikkus);
+    console.log('Laius = ' + laius);
+    //arvutame pindala
+    let pindala = pikkus * laius;
+    console.log('Pindala = ' + pindala);
 }
-
-//Funktsiooni kasutamine
-let paarsus;
-paarsus = paarsuseKontroll(7);
-console.log(paarsus);
-//paarsus= asemel kohe paarsuseKontroll consolis
-console.log(paarsuseKontroll(9));
-paarsuseKontroll(7765);
+pindalaArvutamine(5, 6);
