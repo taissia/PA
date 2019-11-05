9d864dbf4a5f99fdcc91863e5a3d3e401e831462
Taissia George <taissia.george@khk.ee>
2019-11-05 14:54:00 +0200

    väärtuse tagastamine returniga

--- app.js
+++ app.js
@@ -3,21 +3,28 @@
 // function funktsiooniNimi([param1, param2, ...,paramN]){
     //funktsiooni sisu }
 function paarsuseKontroll(arv){
-    
+    //tulemuslause gefineerimine
+    let lause;
 
     //arvutame arv 2 jagamisel
     let jaak = arv % 2;
     //kui jaak võrdne nulliga
     if( jaak === 0 ){
         //tegu on paaris arvuga
-        console.log(arv + ' on paaris');
+     
+        lause = arv + ' on paaris';
     } else {
         //tegu on paaritu arvuga
-        console.log(arv + ' on paaritu ');
+        lause = arv + ' on paaritu ';
     }
+    //tagastame antud väärtus
+    return lause;
 }
 
 //Funktsiooni kasutamine
-paarsuseKontroll(7);
-paarsuseKontroll(9);
+let paarsus;
+paarsus = paarsuseKontroll(7);
+console.log(paarsus);
+//paarsus= asemel kohe paarsuseKontroll consolis
+console.log(paarsuseKontroll(9));
 paarsuseKontroll(7765);
